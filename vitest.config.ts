bae1798d818cbfa68d3df334e5043ec7a153bc10
gwillingham3 <gerardzon@yahoo.com>
2025-05-12 02:27:25 -0400

    Using a relative path for the setup files so that GitHub workflows can find them

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./test/setup.ts'],
+    setupFiles: ['test/setup.ts'],
     testTimeout: 5000,
     environmentOptions: {
       jsdom: {
